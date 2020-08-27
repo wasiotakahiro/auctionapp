f99acea315dfe30fc0cb76b37455fd90f9c85dd6
tanaka ryota <tanaka.ryota@m.alhinc.jp>
2020-08-27 20:35:10 +0900

    add gradle wrapper

--- src/main/java/com/demo/auctionapp/infrastructure/repository/UsersRepository.java
+++ src/main/java/com/demo/auctionapp/infrastructure/repository/UsersRepository.java
@@ -2,7 +2,6 @@ package com.demo.auctionapp.infrastructure.repository;
 
 import com.demo.auctionapp.infrastructure.repository.entity.UsersEntity;
 import com.demo.auctionapp.infrastructure.repository.mapper.UsersMapper;
-import com.demo.auctionapp.service.model.SignUp;
 import com.demo.auctionapp.service.model.Users;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
