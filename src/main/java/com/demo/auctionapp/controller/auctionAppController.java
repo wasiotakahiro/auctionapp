14610a103f225e3110f61c7fd558b49773b78fd8
tanaka ryota <tanaka.ryota@m.alhinc.jp>
2020-09-03 17:13:28 +0900

    add validation

--- src/main/java/com/demo/auctionapp/controller/auctionAppController.java
+++ src/main/java/com/demo/auctionapp/controller/auctionAppController.java
@@ -6,6 +6,8 @@ import com.demo.auctionapp.service.model.SignUp;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,7 +30,10 @@ public class auctionAppController {
     }
 
     @PostMapping("/sign_up")
-    public ModelAndView signUp(@ModelAttribute SignUpRequest request) {
+    public ModelAndView signUp(@ModelAttribute @Validated SignUpRequest request, BindingResult error) {
+        if (error.hasErrors()) {
+            return new ModelAndView("sign_up");
+        }
         log.info("アカウント登録リクエスト：{}", request);
         usersService.signUp(SignUp.create(request));
         return new ModelAndView("product_list");
--- src/main/java/com/demo/auctionapp/controller/resourses/request/SignUpRequest.java
+++ src/main/java/com/demo/auctionapp/controller/resourses/request/SignUpRequest.java
@@ -3,28 +3,45 @@ package com.demo.auctionapp.controller.resourses.request;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.thymeleaf.util.StringUtils;
 
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignUpRequest {
-    @NotNull
+    @NotBlank(message = "名前は入力必須です。")
     private String name;
-    @NotNull
+    @NotBlank(message = "誕生日は入力必須です。")
     private String birthday;
-    @NotNull
+    @NotBlank(message = "メールアドレスは入力必須です。")
     private String eMail;
     private String phoneNumber;
-    @NotNull
+    @NotBlank(message = "住所は入力必須です。")
     private String address;
-    @NotNull
+    @NotBlank(message = "ユーザ名は入力必須です。")
     private String userName;
-    @NotNull
+    @NotBlank(message = "ユーザIDは入力必須です。")
     private String userId;
-    @NotNull
+    @NotBlank(message = "パスワードは入力必須です。")
     private String password;
-    @NotNull
+    @NotBlank(message = "確認用パスワードは入力必須です。")
     private String confirmPassword;
+
+    /**
+     * パスワード相関チェック
+     * @return パスワードと確認用パスワードが一致している場合はtrue
+     */
+    @AssertTrue(message = "パスワードと確認用パスワードが一致していません。")
+    public boolean isPasswordConfirm() {
+        if (StringUtils.isEmpty(password) || StringUtils.isEmpty(confirmPassword)) {
+            // フィールドが空の場合はチェック対象外
+            return true;
+        }
+
+        return password.equals(confirmPassword);
+    }
 }
